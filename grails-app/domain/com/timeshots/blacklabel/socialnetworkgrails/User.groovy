2373f77e3f115de59cbc78e8dd858e3d4df81e4a
redwolfgang24 <redwolfgang20@gmail.com>
2014-07-30 21:39:17 +0800

    update for social network grails

--- grails-app/domain/com/timeshots/blacklabel/socialnetworkgrails/User.groovy
+++ grails-app/domain/com/timeshots/blacklabel/socialnetworkgrails/User.groovy
@@ -2,6 +2,45 @@ package com.timeshots.blacklabel.socialnetworkgrails
 
 class User {
 
+    transient springSecurityService
+    String firstName
+    String lastName
+    String emailAddress
+    String username
+    String password
+    Boolean hasConfirmedEmail = false
+    String id
+
+    static transients = ['springSecurityService']
+
     static constraints = {
+        firstName(nullable: false)
+        lastName(nullable: false)
+        emailAddress(nullable: false, email: true)
+        username(nullable: true, matches:"[a-zA-Z0-9]+")
+        password(nullable: false, password: true)
+    }
+
+    static mapping = {
+        hasConfirmedEmail(defaultValue: false)
+        table 'user'
+        password column: '`password`'
+        id generator:'uuid'
+    }
+
+    def beforeInsert() {
+        if (password != null) {
+            encodePassword()
+        }
+    }
+
+    def beforeUpdate() {
+        if (isDirty('password')) {
+            encodePassword()
+        }
+    }
+
+    protected void encodePassword() {
+        password = springSecurityService.encodePassword(password)
     }
 }
